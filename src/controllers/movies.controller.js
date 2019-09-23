3ad10f7b412dc1368b78cd5bc52bd3c32e3a4b2b
Khoa Tran <khoatran121294@gmail.com>
2019-09-24 01:02:05 +0700

    remove console log & dummy data in movies controller

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -1,5 +1,5 @@
 import { Movie } from "../models/movies";
-import { movies as dummyMovies } from "../../dummy-data/movies";
+// import { movies as dummyMovies } from "../../dummy-data/movies";
 
 export const getMovies = async (req, res, next) => {
   const movies = await Movie.findAll();
@@ -10,7 +10,6 @@ export const addMovie = async (req, res, next) => {
   const { title, linkUrl, description } = req.body;
   // youtube refuses direct link, so I will replace the link
   const _linkUrl = linkUrl.replace("watch?v=", "embed/");
-  console.log("_linkUrl", _linkUrl);
   const newMovie = await Movie.create({
     title,
     linkUrl: _linkUrl, 
